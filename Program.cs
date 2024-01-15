f132241aba50da87f8faa40c32947513095ca184
singhet <singhet@SCSWL4072>
2024-01-15 14:02:23 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Program liczy deltę dla równania kwadratowego ax^2 + bx + c = 0
+        // po otrzymaniu od użytkownia a,b,c wyrażonych w wartościach liczbowych
+        //The program calculates the delta for the quadratic equation ax^2 + bx + c = 0
+        //after receiving a, b, c expressed in numerical values from the user
+        double a, b, c, wynik;
+        Console.WriteLine("Let's calculate quadratic equation: ax^2 + bx + c = 0");
+        Console.WriteLine("Enter the value of \"a\"and press Enter");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the value of \"b\"and press Enter");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the value of \"b\"and press Enter");
+        c = double.Parse(Console.ReadLine());
+        wynik = (Math.Pow(b, 2) - (4 * a * c ));
+        Console.WriteLine("The result is: " + wynik);
+        Console.ReadKey();
+
+
+
+
+
+
+
+
+
+   }
+}
